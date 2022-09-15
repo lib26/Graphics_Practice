7638ee8addd8e263c15cdbc0b7563ca7af59e70d
lib26 <dladlsqja@naver.com>
2022-09-15 14:27:51 +0900

    shader

---
+++ 2_tree/TREE.js
@@ -0,0 +1,52 @@
+
+var gl;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    
+
+    // Associate vertex data buffer with shader variables
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+    
+    var uColor=gl.getUniformLocation(program,"uColor");
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    var body = new Float32Array([
+        0, 1, -0.5, 0.5, 0.5, 0.5,
+        0, 0.5, -0.5, 0, 0.5, 0,
+        0, 0, -0.5, -0.5, 0.5, -0.5,
+        -0.15, -0.5, 0.15, -0.5, -0.15, -1,
+        0.15, -0.5, -0.15, -1, 0.15, -1
+    ]);
+
+    gl.bufferData(gl.ARRAY_BUFFER, body, gl.STATIC_DRAW);
+
+    gl.uniform4fv(uColor,[0,1,0,1]);
+    gl.drawArrays( gl.TRIANGLES, 0, 9 );
+
+    gl.uniform4fv(uColor, [0.5,0.25,0,1]);
+    gl.drawArrays( gl.TRIANGLES, 9, 6 );
+    
+};
+
+
